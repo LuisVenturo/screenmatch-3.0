b55bc16680f0446cf18813880673f8a8e7c3c8e5
Luis Venturo <venturomoralesluis@gmail.com>
2024-09-19 07:16:22 -0500

    Suministrando datos para front

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,20 +1,28 @@
 package com.aluracursos.screenmatch.controller;
 
-import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.dto.SerieDTO;
 import com.aluracursos.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+//@RequestMapping("/api/v1/serie")
 public class SerieController {
     @Autowired
     SerieRepository serieRepository;
     @GetMapping("/series")
-    public List<Serie>  obtenerTodasLasSeries(){
-        return serieRepository.findAll();
+    public List<SerieDTO>  obtenerTodasLasSeries(){
+        return serieRepository.findAll().stream()
+                .map(serie -> new SerieDTO(serie.getTitulo(), serie.getTotalTemporadas(), serie.getEvaluacion(), serie.getPoster(), serie.getGenero(),
+                        serie.getActores(), serie.getSinopsis()))
+                .collect(Collectors.toList());
     }
+
+
 
 }
